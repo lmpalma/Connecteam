38ba00991abc1650d93eda5cb65e5ea3be598bff
Maui Pinero <mauipinero2003@gmail.com>
2024-10-01 21:40:31 +0800

    added landing page

---
+++ resources/views/frontend/master.blade.php
@@ -0,0 +1,306 @@
+<!DOCTYPE html>
+<html lang="zxx" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <title>Connecteam - An Employee Task Management Platform</title>
+    <meta name="description" content="Ankon is a free bootstrap theme by themes for app. We have made it for the productized services startup and enntrepreneurs. It includes all essential sections that a startup might need.">
+    <meta name="keywords" content="html template, free bootstrap theme, themes for app, themesforapp, productized service, productised service">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Standard favicon -->
+    <link rel="icon" type="image/png" href="./assets/images/favicon.ico">
+    <!-- Helps prevent duplicate content issues -->
+    <link rel="canonical" href="https://themesfor.app/themes/ankon/">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!-- Facebook Open Graph -->
+    <meta property="og:title" content="Ankon, a free HTML template for productized services startup" />
+    <meta property="og:image" content="https://themesfor.app/all-themes/assets/images/ankon.png" />
+    <meta property="og:site_name" content="Themes For App" />
+    <meta property="og:description" content="Ankon is a free bootstrap theme by themes for app. We have made it for the productized services startup and enntrepreneurs. It includes all essential sections that a startup might need." />
+    <!-- Twitter Card -->
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:url" content="https://themesfor.app/themes/ankon/">
+    <meta name="twitter:title" content="Ankon, a free HTML template for productized services startup">
+    <meta name="twitter:description" content="Ankon is a free bootstrap theme by themes for app. We have made it for the productized services startup and enntrepreneurs. It includes all essential sections that a startup might need.">
+    <meta name="twitter:image" content="https://themesfor.app/all-themes/assets/images/ankon.png">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="./assets/vendors/bootstrap/css/bootstrap.min.css">
+    <!-- Main CSS -->
+    <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="cover">
+            <!-- Header -->
+            <header class="header">
+                <div class="container">
+                    <nav class="navbar navbar-expand-lg align-content-center px-0">
+                        <a class="navbar-brand logo" href="#">Connecteam</a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu" aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation">
+                            <i class="fas fa-bars"></i>
+                        </button>
+
+                        <div class="collapse navbar-collapse" id="menu">
+                            <ul class="navbar-nav ml-lg-auto">
+                                <li class="nav-item">
+                                    <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link ips" href="#features">Features</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-primary" href="#">Login</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </header>
+            <!--/ End Header -->
+            <!-- Hero -->
+            <div class="jumbotron jumbotron-fluid bg-transparent hero section">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-12 col-lg-10 col-xl-9 mx-auto">
+                            <div class="hero-content text-center">
+                                <h1 class="hero-title">Efficient Employee Task Management</h1>
+                                <!-- Empty Space -->
+                                <div class="space-md"></div>
+                                <!--/ End Empty Space -->
+                                <p class="lead lead-lg">Connecteam is your all-in-one employee task management solution. Designed to optimize task allocation, track progress, and improve collaboration, it empowers your team to stay on top of their work. Whether you're managing a growing startup or an established small business, Connecteam simplifies how you assign, monitor, and complete tasks.</p>
+                                <!-- Empty Space -->
+                                <div class="space-lg"></div>
+                                <!--/ End Empty Space -->
+                                <a href="#" class="btn btn-lg btn-primary btn-outline-primary">LOG IN</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--/ End Hero -->
+        </div>
+        <!-- Feature -->
+        <section id="features" class="section feature">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="row">
+                            <div class="col-12 col-md-4">
+                                <div class="single-feature text-center text-lg-left">
+                                    <img src="./assets/images/feature-1.png" alt="feature" class="img-fluid feature-img">
+                                    <!-- Empty Space -->
+                                    <div class="space-md"></div>
+                                    <!--/ End Empty Space -->
+                                    <h3 class="title">Task Management</h3>
+                                    <!-- Empty Space -->
+                                    <div class="space-sm"></div>
+                                    <!--/ End Empty Space -->
+                                    <p class="lead">
+                                    Easily assign and track tasks in one place.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-4">
+                                <div class="single-feature text-center text-lg-left">
+                                    <img src="./assets/images/feature-2.png" alt="feature" class="img-fluid feature-img">
+                                    <!-- Empty Space -->
+                                    <div class="space-md"></div>
+                                    <!--/ End Empty Space -->
+                                    <h3 class="title">Progress Tracking</h3>
+                                    <!-- Empty Space -->
+                                    <div class="space-sm"></div>
+                                    <!--/ End Empty Space -->
+                                    <p class="lead">
+                                    Monitor task completion in real-time.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-4">
+                                <div class="single-feature text-center text-lg-left">
+                                    <img src="./assets/images/feature-3.png" alt="feature" class="img-fluid feature-img">
+                                    <!-- Empty Space -->
+                                    <div class="space-md"></div>
+                                    <!--/ End Empty Space -->
+                                    <h3 class="title">User Management</h3>
+                                    <!-- Empty Space -->
+                                    <div class="space-sm"></div>
+                                    <!--/ End Empty Space -->
+                                    <p class="lead">
+                                    Organize your team and streamline workflows.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Empty Space -->
+                        <div class="space-md"></div>
+                        <!--/ End Empty Space -->
+                        <div class="row">
+                            <div class="col-12 col-lg-11 mr-lg-auto">
+                                <h3 class="title">Benefits of Using Connecteam</h3>
+                                <!-- Empty Space -->
+                                <div class="space-md"></div>
+                                <!--/ End Empty Space -->
+                            </div>
+                            <div class="col-12 col-md-6 col-lg-4">
+                                <div class="single-feature mt-4 text-center text-lg-left">
+                                    <div class="row align-items-start">
+                                        <div class="col pr-lg-0">
+                                            <img src="./assets/images/thumb/icon-1.png" alt="feautes" class="icon">
+                                        </div>
+                                        <div class="col-12 col-lg-10 mt-4 mt-lg-0 mr-auto">
+                                            <h4 class="title">Enhanced Productivity</h4>
+                                            <!-- Empty Space -->
+                                            <div class="pt-1"></div>
+                                            <!--/ End Empty Space -->
+                                            <p class="lead">
+                                            With a streamlined task assignment process, team members can focus on what matters most, reducing time spent on administrative tasks and increasing overall productivity.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6 col-lg-4">
+                                <div class="single-feature mt-4 text-center text-lg-left">
+                                    <div class="row align-items-start">
+                                        <div class="col pr-lg-0">
+                                            <img src="./assets/images/thumb/icon-2.png" alt="feautes" class="icon">
+                                        </div>
+                                        <div class="col-12 col-lg-10 mt-4 mt-lg-0 mr-auto">
+                                            <h4 class="title">Improved Collaboration</h4>
+                                            <!-- Empty Space -->
+                                            <div class="pt-1"></div>
+                                            <!--/ End Empty Space -->
+                                            <p class="lead">
+                                            Connecteam simplifies team coordination by providing a clear overview of tasks and responsibilities. Team members can easily see who is working on what, ensuring everyone is aligned and tasks are completed efficiently.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6 col-lg-4">
+                                <div class="single-feature mt-4 text-center text-lg-left">
+                                    <div class="row align-items-start">
+                                        <div class="col pr-lg-0">
+                                            <img src="./assets/images/thumb/icon-3.png" alt="feautes" class="icon">
+                                        </div>
+                                        <div class="col-12 col-lg-10 mt-4 mt-lg-0 mr-auto">
+                                            <h4 class="title">Real-Time Visibility</h4>
+                                            <!-- Empty Space -->
+                                            <div class="pt-1"></div>
+                                            <!--/ End Empty Space -->
+                                            <p class="lead">
+                                            Managers can monitor task progress in real-time, allowing for quick adjustments and ensuring that projects stay on track. This visibility helps identify potential roadblocks before they become issues.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6 col-lg-4">
+                                <div class="single-feature mt-4 text-center text-lg-left">
+                                    <div class="row align-items-start">
+                                        <div class="col pr-lg-0">
+                                            <img src="./assets/images/thumb/icon-4.png" alt="feautes" class="icon">
+                                        </div>
+                                        <div class="col-12 col-lg-10 mt-4 mt-lg-0 mr-auto">
+                                            <h4 class="title">Customizable Workflows</h4>
+                                            <!-- Empty Space -->
+                                            <div class="pt-1"></div>
+                                            <!--/ End Empty Space -->
+                                            <p class="lead">
+                                            Connecteam allows businesses to tailor workflows to meet their unique needs. Whether it's different teams or projects, you can customize task management processes to fit your organization’s structure.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6 col-lg-4">
+                                <div class="single-feature mt-4 text-center text-lg-left">
+                                    <div class="row align-items-start">
+                                        <div class="col pr-lg-0">
+                                            <img src="./assets/images/thumb/icon-5.png" alt="feautes" class="icon">
+                                        </div>
+                                        <div class="col-12 col-lg-10 mt-4 mt-lg-0 mr-auto">
+                                            <h4 class="title">Data-Driven Insights</h4>
+                                            <!-- Empty Space -->
+                                            <div class="pt-1"></div>
+                                            <!--/ End Empty Space -->
+                                            <p class="lead">
+                                            By utilizing reporting and analytics features, businesses can gain valuable insights into team performance and task completion rates. This data helps in making informed decisions and optimizing future workflows.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--/ End Feature -->
+        <!-- Feature -->
+        <section class="section feature">
+            <div class="container">
+                <!-- Empty Space -->
+                <div class="space-md"></div>
+                <!--/ End Empty Space -->
+                <div class="row">
+                    <div class="col-12 col-lg-9 text-center mx-auto">
+                        <h2>
+                            About Us
+                        </h2>
+                        <!-- Empty Space -->
+                        <div class="space-md"></div>
+                        <!--/ End Empty Space -->
+                        <p class="lead lead-lg">
+                        Connecteam is all about making work easier. We provide a simple platform for businesses to manage tasks, improve teamwork, and stay organized. Our goal is to help teams work smarter and get more done every day. Join us and see how Connecteam can simplify your workflow.
+                        </p>
+                    </div>
+                </div>
+                <!-- Empty Space -->
+                <div class="space-lg"></div>
+                <div class="space-md"></div>
+                <!--/ End Empty Space -->
+            </div>
+        </section>
+        <!--/ End Feature -->
+        
+        <!-- Footer -->
+        <footer class="footer section" style="background-color: #5b3a9b;">
+            <div class="container">
+                <div class="row align-items-center">
+                <div class="col-12 col-lg-12 d-flex justify-content-center align-items-center" style="height: 100px;">
+                    <div class="copyright text-center" style="color: white;">
+                        &copy; 2024, Connecteam. All rights reserved.
+                    </div>
+                </div>
+                    <!-- <div class="col-12 col-lg-6">
+                        <ul class="nav justify-content-center justify-content-lg-end">
+                            <li class="nav-item"><a href="#" class="nav-link"><i class="fab fa-facebook-f"></i></a></li>
+                            <li class="nav-item"><a href="#" class="nav-link"><i class="fab fa-twitter"></i></a></li>
+                            <li class="nav-item"><a href="#" class="nav-link"><i class="fab fa-instagram"></i></a></li>
+                        </ul>
+                    </div> -->
+                </div>
+            </div>
+        </footer>
+        <!--/ End Footer -->
+    </div>
+    <!-- jQuery Script -->
+    <script src="./assets/vendors/jquery/jquery.min.js"></script>
+    <!-- Popper Script -->
+    <script src="./assets/vendors/bootstrap/js/popper.min.js"></script>
+    <!-- Bootstrap Script -->
+    <script src="./assets/vendors/bootstrap/js/bootstrap.min.js"></script>
+    <!-- Main Script -->
+    <script src="./assets/js/main.js"></script>
+</body>
+</html>
