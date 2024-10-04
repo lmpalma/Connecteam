7a636dcee97ecf7a1fa2b8a40243dc2f76c3cdae
kazeuha <mara.pinero.up@phinmaed.com>
2024-10-04 14:00:58 +0800

    updated designs for responsiveness

--- resources/views/frontend/login.blade.php
+++ resources/views/frontend/login.blade.php
@@ -1,167 +1,168 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="icon" href="/" type="image/x-icon">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="/" type="image/x-icon">
+    <title>Connecteam - LOGIN</title>
+    <style>
+        body {
+            min-height: 100vh;
+            background-image: url("./assets/images/bg1.png");
+            background-repeat: no-repeat;
+            background-size: cover;
+            margin: 0;
+            display: flex;
+            flex-direction: column;
+        }
+        .header {
+            width: 100%;
+            border-bottom: solid gray 2px;
+            height: 10vh;
+            display: flex;
+            align-items: center;
+            background: rgba(255, 255, 255, 0.8);
+            z-index: 1;
+            padding: 0 20px;
+        }
+        .web-img {
+            width: 50px;
+            height: 50px;
+            border-radius: 50%;
+        }
+        .web-title {
+            margin-left: 15px;
+            font-size: 24px; 
+            font-family: Verdana;
+            color: #5b3a9b;
+        }
+        .page-area {
+            flex-grow: 1;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            padding: 20px;
+        }
+        .container {
+            width: 90%;
+            max-width: 400px;
+            padding: 40px;
+            background-color: white;
+            border-radius: 20px;
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+        label {
+            font-size: 14px;
+            font-family: Verdana;
+            color: rgb(0, 0, 0);
+            align-self: flex-start;
+            margin-bottom: 5px;
+        }
+        input {
+            width: 100%;
+            height: 40px; 
+            font-size: 16px;
+            margin: 10px 0; 
+            padding: 0 10px; 
+            background: rgba(155, 62, 164, 0.5);
+            border: 2px solid rgb(100, 0, 153);
+            border-radius: 10px;
+            outline: none;
+        }
+        button {
+            margin-top: 15px;
+            width: 100%;
+            height: 40px;
+            font-size: 16px; 
+            color: white;
+            background-color: rgb(100, 0, 153);
+            border-radius: 10px;
+            cursor: pointer;
+            border: none;
+            transition: background-color 0.3s;
+        }
+        button:hover {
+            background-color: rgb(0, 0, 0);
+        }
+        p {
+            font-size: 12px;
+            font-family: Verdana;
+            color: rgb(0, 0, 0);
+            text-align: center;
+            margin: 10px 0;
+        }
+        a {
+            font-style: italic;
+            color: rgb(0, 0, 0);
+        }
+        .footer-bar {
+            height: 10vh;
+            width: 100%;
+            border-top: solid gray 2px;
+            text-align: center;
+        }
+        h4 {
+            font-size: 12px;
+            padding: 20px;
+            color: white;
+            font-family: Georgia;
+        }
+        .welcome {
+            font-size: 35px;
+            font-family: Tahoma;
+            text-align: center;
+            margin: 0;
+            margin-bottom: 20px;
+        }
+        .intro {
+            font-size: 18px;
+            font-family: Tahoma;
+            text-align: center;
+            margin: 0;
+            margin-bottom: 30px; 
+        }
 
-        <title>Connecteam - LOGIN</title>
-        <style>
-            body{
-                min-height: 100vh;
-                background-image: url("./assets/images/bg1.png");
-                background-repeat: none;
-                background-size: cover;
-            }
-            .page{
-                width: 100%;
-                height: 100%;
-            }
-            .header{
-                width: 100%;
-                border-bottom: solid gray 2px;
-                height: 10vh;
-                margin-top: 0;
-                margin-bottom: 5px;
-                display: flex;
-            }
-            .web-img{
-                margin-left: 20px;
-                padding: 10px;
-                width: 50px;
-                height: 50px;
-                border-radius: 50%;
-            }
-            .web-title{
-                padding: 10px;
-                justify-content: space-around;
-                margin-left: 15px;
-                align-items: center;
-                font-size: 20px;
-                font-family: Verdana;
-                color: #5b3a9b;
-            }
-            .page-area{
-                padding: 10px;
-                margin-top: 10px;
-                margin-bottom: 10px;
-                
-            }
-            .container{
-                width: 25vw;
-                height: 32vh;
+        @media (max-width: 768px) {
+            .container {
+                width: 90%;
                 padding: 30px;
-                margin-left: auto;
-                margin-right: auto;
-                background-color: white;
-                border-radius: 20px;
-                margin-top: 50px;
-                margin-bottom: 90px;
-                box-shadow: black 0 0 5px;
             }
-            .container-name{
-                font-size: 20px;
-                font-family: Verdana;
-                text-align: center;
-                color: rgb(0, 0, 0);
+            .welcome {
+                font-size: 30px;
             }
-            label{
-                font-size: 14px;
-                font-family: Verdana;
-                color: rgb(0, 0, 0);
+            .intro {
+                font-size: 16px;
             }
-            input{
-                width: 23vw;
-                margin: 2px;
-                margin-left: 10px;
-                height: 25px;
-                font-size: 14px;
-                color: black;
-                background: rgba(155, 62, 164, 0.5);
-                border-radius: 10px;
-            }
-            button{
-                margin-inline-start: 25%;
-                width: 50%;
-                height: 30px;
-                font-size: 15px;
-                color: white;
-                background-color: rgb(100, 0, 153);
-                border-radius: 10px;
-            }
-            button:hover{
-                background-color: rgb(0, 0, 0);
-                color: white;
-                cursor: pointer;
-            }
-            p{
-                font-size: 12px;
-                font-family: Verdana;
-                text-align: center;
-                color: rgb(0, 0, 0);
-                text-shadow: black 2px 2px 5px;
-            }
-            a{
-                font-style: italic;
-                color: rgb(0, 0, 0);
-            }
-            .footer-bar{
-                margin-bottom: 0;
-                height: 10vh;
-                width: 100%;
-                border-top: solid gray 2px;
-                margin-top: -10px;
-            }
-            h4{
-                font-size: 12px;
-                text-align: center;
-                padding: 20px;
-                color:white;
-                font-family: Georgia;
-            }
-            .welcome{
-                font-size: 45px;
-                text-align: center;
-                font-family: Tahoma;
-            }
-            .intro{
-                margin-top: -35px;
-                font-size: 20px;
-                text-align: center;
-                font-family: Tahoma;
-                text-shadow: none;
-            }
-        </style>
-    </head>
-    <body>
-        <div class = "page">
-            <div class = "header">
-                <!-- <div class = "navigation"></div> -->
-                <img src = "./assets/images/icon.png" class = "web-img">
-                <h1 class = "web-title">Connecteam</h1>
-            </div>
-            <div class = "page-area">
-                <h1 class = "welcome">WELCOME BACK!</h1>
-                <p class = "intro">We are glad to see you again.</p>
-                <div class = "container">
-                    <label>Email:</label><br>
-                    <input type = "text" class = "input-box">
-                    <br><br>
-                    <label>Password:</label><br>
-                    <input type = "password"  class = "input-box">
-                    <p>Forgot password? <a href = "signup.html">Click Here</a></p>
-                    
-                    <p>Don't have an account? <a href = "{{ route('frontend.signup') }}">Sign Up</a></p>
-                    <br>
-                    <button type ="button" class = "login-btn">LOGIN</button>
-                </div>
-            </div>
+        }
+    </style>
+</head>
+<body>
+    <div class="header">
+        <img src="./assets/images/icon.png" class="web-img" alt="Logo">
+        <h1 class="web-title">Connecteam</h1>
+    </div>
+    <div class="page-area">
+        <h1 class="welcome">WELCOME BACK!</h1>
+        <p class="intro">We are glad to see you again.</p>
+        <div class="container">
+            <label for="email">Email:</label>
+            <input type="text" id="email" class="input-box" placeholder="Enter your email" required>
+            <label for="password">Password:</label>
+            <input type="password" id="password" class="input-box" placeholder="Enter your password" required>
+            <p>Forgot password? <a href="signup.html">Click Here</a></p>
+            <p>Don't have an account? <a href="{{ route('frontend.signup') }}">Sign Up</a></p>
+            <button type="button" class="login-btn">LOGIN</button>
         </div>
-    </body>
+    </div>
     <footer>
-        <div class = "footer-bar">
+        <div class="footer-bar">
             <h4>©️ 2024 - Connecteam | All rights reserved.</h4>
         </div>
     </footer>
+</body>
 </html>
--- resources/views/frontend/signup.blade.php
+++ resources/views/frontend/signup.blade.php
@@ -1,162 +1,166 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="icon" href="/" type="image/x-icon">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="/" type="image/x-icon">
+    <title>Connecteam - SIGNUP</title>
+    <style>
+        body {
+            min-height: 100vh;
+            background-image: url("./assets/images/bg1.png");
+            background-repeat: no-repeat;
+            background-size: cover;
+            margin: 0;
+            display: flex;
+            flex-direction: column;
+        }
+        .header {
+            width: 100%;
+            border-bottom: solid gray 2px;
+            height: 10vh;
+            display: flex;
+            align-items: center;
+            background: rgba(255, 255, 255, 0.8);
+            z-index: 1;
+            padding: 0 20px;
+        }
+        .web-img {
+            width: 50px;
+            height: 50px;
+            border-radius: 50%;
+        }
+        .web-title {
+            margin-left: 15px;
+            font-size: 24px;
+            font-family: Verdana;
+            color: #5b3a9b;
+        }
+        .page-area {
+            flex-grow: 1;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            padding: 20px;
+        }
+        .container {
+            width: 90%;
+            max-width: 400px;
+            padding: 40px;
+            background-color: white;
+            border-radius: 20px;
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+        .container-name {
+            font-size: 18px;
+            font-family: Verdana;
+            text-align: center;
+            color: rgb(0, 0, 0);
+            margin-bottom: 20px;
+        }
+        label {
+            font-size: 14px;
+            font-family: Verdana;
+            color: rgb(0, 0, 0);
+            align-self: flex-start; 
+            margin-bottom: 5px;
+        } 
+        input {
+            width: 100%; 
+            height: 40px;
+            font-size: 16px;
+            margin: 10px 0; 
+            padding: 0 10px;
+            background: rgba(155, 62, 164, 0.5);
+            border: 2px solid rgb(100, 0, 153);
+            border-radius: 10px;
+            outline: none;
+        }
+        button {
+            margin-top: 15px;
+            width: 100%; 
+            height: 40px;
+            font-size: 16px;
+            color: white;
+            background-color: rgb(100, 0, 153);
+            border-radius: 10px;
+            cursor: pointer;
+            border: none;
+            transition: background-color 0.3s;
+        }
+        button:hover {
+            background-color: rgb(0, 0, 0);
+        }
+        p {
+            font-size: 12px;
+            font-family: Verdana;
+            text-align: center;
+            color: rgb(0, 0, 0);
+            margin: 10px 0;
+        }
+        a {
+            font-style: italic;
+            color: rgb(0, 0, 0);
+        }
+        .footer-bar {
+            height: 10vh;
+            width: 100%;
+            border-top: solid gray 2px;
+            text-align: center;
+        }
+        h4 {
+            font-size: 12px;
+            padding: 20px;
+            color: white;
+        }
+        .welcome {
+            font-size: 35px;
+            font-family: Tahoma;
+            text-align: center;
+            margin: 0;
+            margin-bottom: 20px;
+        }
 
-        <title>Connecteam - SIGNUP</title>
-        <style>
-            body{
-                min-height: 100vh;
-                background-image: url("./assets/images/bg1.png");
-                background-repeat: none;
-                background-size: cover;
+        @media (max-width: 768px) {
+            .container {
+                width: 90%;
+                padding: 30px;
             }
-            .page{
-                width: 100%;
-                height: 100%;
+            .welcome {
+                font-size: 30px;
             }
-            .header{
-                width: 100%;
-                border-bottom: solid gray 2px;
-                height: 10vh;
-                margin-top: 0;
-                margin-bottom: 5px;
-                display: flex;
-            }
-            .web-img{
-                margin-left: 20px;
-                padding: 10px;
-                width: 50px;
-                height: 50px;
-                border-radius: 50%;
-            }
-            .web-title{
-                padding: 10px;
-                justify-content: space-around;
-                margin-left: 15px;
-                align-items: center;
-                font-size: 20px;
-                font-family: Verdana;
-                color: #5b3a9b;
-            }
-            .page-area{
-                padding: 10px;
-                margin-top: 10px;
-                margin-bottom: 10px;
-                
-            }
-            .container{
-                width: 25vw;
-                height: 50vh;
-                padding: 40px;
-                margin-left: auto;
-                margin-right: auto;
-                background-color: white;
-                border-radius: 20px;
-                margin-top: 30px;
-                margin-bottom: 30px;
-                box-shadow: black 0 0 5px;
-            }
-            .container-name{
-                font-size: 18px;
-                font-family: Verdana;
-                text-align: center;
-                color: rgb(0, 0, 0);
-            }
-            label{
-                font-size: 14px;
-                font-family: Verdana;
-                color: rgb(0, 0, 0);
-            } 
-            input{
-                width: 23vw;
-                margin: 2px;
-                margin-left: 10px;
-                height: 25px;
-                font-size: 14px;
-                color: black;
-                background: rgba(155, 62, 164, 0.5);
-                border-radius: 10px;
-            }
-            button{
-                margin-inline-start: 25%;
-                width: 50%;
-                height: 30px;
-                font-size: 15px;
-                color: white;
-                background-color: rgb(100, 0, 153);
-                border-radius: 10px;
-            }
-            button:hover{
-                background-color: rgb(0, 0, 0);
-                color: white;
-                cursor: pointer;
-            }
-            p{
-                font-size: 12px;
-                font-family: Verdana;
-                text-align: center;
-                color: rgb(0, 0, 0);
-                text-shadow: black 2px 2px 5px;
-            }
-            a{
-                font-style: italic;
-                color: rgb(0, 0, 0);
-            }
-            .footer-bar{
-                margin-bottom: 0;
-                height: 10vh;
-                width: 100%;
-                border-top: solid gray 2px;
-                margin-top: -10px;
-            }
-            h4{
-                font-size: 12px;
-                text-align: center;
-                padding: 20px;
-                color:white;
-            }
-            .welcome{
-                font-size: 45px;
-                text-align: center;
-                font-family: Tahoma;
-            }
-        </style>
-    </head>
-    <body>
-        <div class = "page">
-            <div class = "header">
-                <!-- <div class = "navigation"></div> -->
-                 <img src = "./assets/images/icon.png" class = "web-img">
-                <h1 class = "web-title">Connecteam</h1>
-            </div>
-            <div class = "page-area">
-                <h1 class = "welcome">CREATE AN ACCOUNT</h1>
-                <div class = "container">
-                    <label>Full Name</label><br>
-                    <input type = "text" class = "input-box">
-                    <br><br>
-                    <label>Email</label><br>
-                    <input type = "text"  class = "input-box">
-                    <br><br>
-                    <label>Password</label><br>
-                    <input type = "password"  class = "input-box">
-                    <br><br>
-                    <label>Confirm Password</label><br>
-                    <input type = "password"  class = "input-box">
-                    <br><br><br>
-                    <button type ="button" class = "login-btn">SIGN UP</button>
-                    <p>Already have an account? <a href = "{{ route('frontend.login') }}">Log In</a></p>
-                </div>
-            </div>
+        }
+    </style>
+</head>
+<body>
+    <div class="header">
+        <img src="./assets/images/icon.png" class="web-img" alt="Logo">
+        <h1 class="web-title">Connecteam</h1>
+    </div>
+    <div class="page-area">
+        <h1 class="welcome">CREATE AN ACCOUNT</h1>
+        <div class="container">
+            <label for="fullName">Full Name:</label>
+            <input type="text" id="fullName" class="input-box" placeholder="Enter your full name" required>
+            <label for="email">Email:</label>
+            <input type="text" id="email" class="input-box" placeholder="Enter your email" required>
+            <label for="password">Password:</label>
+            <input type="password" id="password" class="input-box" placeholder="Enter your password" required>
+            <label for="confirmPassword">Confirm Password:</label>
+            <input type="password" id="confirmPassword" class="input-box" placeholder="Confirm your password" required>
+            <button type="button" class="login-btn">SIGN UP</button>
+            <p>Already have an account? <a href="{{ route('frontend.login') }}">Log In</a></p>
         </div>
-    </body>
+    </div>
     <footer>
-        <div class = "footer-bar">
+        <div class="footer-bar">
             <h4>©️ 2024 - Connecteam | All rights reserved.</h4>
         </div>
     </footer>
+</body>
 </html>
